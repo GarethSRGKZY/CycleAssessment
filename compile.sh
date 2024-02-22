0fed27dbfb271e142a679042864f697164011347
SequentialEntropy <asahi79289@gmail.com>
2024-02-22 01:40:19 +0000

    add javadoc to compile scripts

--- compile.sh
+++ compile.sh
@@ -2,7 +2,10 @@ mkdir bin
 mkdir build
 mkdir doc
 
+javadoc -d doc/ src/cycling/*.java
+
 javac -d bin/ src/cycling/*.java
+
 jar cvf build/cycling.jar -C bin .
 jar uvf build/cycling.jar -C src .
 jar uvf build/cycling.jar doc
