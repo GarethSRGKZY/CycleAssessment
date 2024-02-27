024d7303a43eb7209a089afa4670a32f31ded611
GarethSRGKZY <divinegareth1313@gmail.com>
2024-02-28 01:19:05 +0800

    implement Stage class

---
+++ src/cycling/Stage.java
@@ -0,0 +1,97 @@
+package cycling;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Stage {
+    //Static Attributes
+    private static int nextId = 0;
+
+    //Static Methods
+    public static int[] getStageIds(ArrayList<Stage> stageInstances) {
+        int size = stageInstances.size();
+
+        int[] result = new int[size]; 
+
+        for (int i = 0; i < size; i++) {
+            result[i] = stageInstances.get(i).getId();
+        }
+
+        return result;
+    }
+
+    public static Stage getStageById(ArrayList<Stage> stageInstances, int id) {
+        return stageInstances.get(Integer.valueOf(id));
+    }
+
+    public static void removeStageById(ArrayList<Stage> stageInstances, int id) {
+        stageInstances.remove(getStageById(stageInstances, id));
+    }
+
+    public static Stage createStage(ArrayList<Stage> stageInstances, String name, String description, double length, LocalDateTime startTime, StageType type) {
+        Stage stage = new Stage(name, description, length, startTime, type);
+        stageInstances.add(stage);
+        return stage;
+    }
+
+    //TODO loadStages()
+    //TODO saveStages()
+
+
+    //Instance Attributes
+    private int id = 0;
+    private ArrayList<Integer> checkpoints;
+    private String name;
+    private String description;
+    private double length;
+    private LocalDateTime startTime;
+    private StageType type; 
+
+    //Instance Methods.
+    private Stage(String name, String description, double length, LocalDateTime startTime, StageType type) {
+        this.id = nextId++;
+        this.checkpoints = new ArrayList<>();
+
+        this.name = name;
+        this.description = description;
+
+        this.length = length;
+        this.startTime = startTime;
+        this.type = type;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public int[] getCheckpoints() {
+        int size = this.checkpoints.size();
+        int[] result = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            result[i] = this.checkpoints.get(i);
+        }
+
+        return result;
+    }
+
+    public void addCheckpoint(int id) {
+        this.checkpoints.add(id);
+    }
+
+    public void removeCheckpoint(int id) {
+        this.checkpoints.remove(Integer.valueOf(id));
+    }
+
+    public String toString() {
+        String checkpoints = Arrays.toString(this.getCheckpoints());
+        return "Race[id=%d, checkpoints=%s, name=%s, description=%s, length=%s, startTime=%s, type=%s]".formatted(this.id, checkpoints, this.name, this.description, this.length, this.startTime, this.type);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+}
