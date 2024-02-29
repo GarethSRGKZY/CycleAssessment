d24bd90590b7fa25e66c6893dc3afefa277ae6a0
SequentialEntropy <asahi79289@gmail.com>
2024-02-29 14:09:20 +0000

    add StageState enum

---
+++ src/cycling/StageState.java
@@ -0,0 +1,22 @@
+package cycling;
+
+/**
+ * This enum is used to represent the stage states on road races.
+ * 
+ * @author 730049785
+ * @version 1.0
+ *
+ */
+public enum StageState {
+	
+	/**
+	 * Used for when Checkpoints are being added & modified.
+	 */
+	PREPARING_STAGE,
+	
+	/**
+	 * Used for when Checkpoints are finalised and when the Stage is accepting results.
+	 */
+	WAITING_FOR_RESULTS,
+	
+}
