GarethSRGKZY/CycleAssessment

commit 7a6f8dba0aa0765820b09ac342b4ebfe113b43a4

implement removeRaceByName in CyclingPortalImpl

src/cycling/CyclingPortalImpl.java
@@ -404,8 +404,16 @@ public class CyclingPortalImpl implements CyclingPortal {
 
 	@Override
 	public void removeRaceByName(String name) throws NameNotRecognisedException {
-		// TODO Auto-generated method stub
+		Race race = Race.findRaceByName(this.raceInstances, name);
+        this.raceInstances.remove(race);
 
+        assert this.raceInstances.contains(race)
+            : "The Race selected for removal should exist in this.raceInstances";
+
+        this.raceInstances.remove(race);
+
+        assert !this.raceInstances.contains(race)
+            : "There should not be any references to a removed Race";
 	}
 
 	@Override

commit 9f6cf4e0214aa6e0de24e5677c2bb7bacf1962f5

fix #19 by removing duplicate teamInstances.remove caused by 7a6f8dba0aa0765820b09ac342b4ebfe113b43a4

src/cycling/CyclingPortalImpl.java
@@ -289,7 +289,6 @@ public class CyclingPortalImpl implements CyclingPortal {
 	@Override
 	public void removeTeam(int teamId) throws IDNotRecognisedException {
 		Team team = findTeam(teamId);
-        this.teamInstances.remove(team);
 
         assert this.teamInstances.contains(team)
             : "The Team selected for removal should exist in this.teamInstances";