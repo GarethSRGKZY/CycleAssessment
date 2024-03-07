GarethSRGKZY/CycleAssessment

commit 3a7656cf0d73605fd89748df6b85597175e9ebe3

add assertions to creation and removal of objects

src/cycling/CyclingPortalImpl.java
@@ -95,8 +95,12 @@ public class CyclingPortalImpl implements CyclingPortal {
 	@Override
 	public int createRace(String name, String description) throws IllegalNameException, InvalidNameException {
         try {
-            Race race = Race.findRaceByName(this.raceInstances, name);
-            if (race instanceof Race) {
+            Race existingRace = Race.findRaceByName(this.raceInstances, name);
+
+            if (existingRace instanceof Race) {
+                assert this.raceInstances.contains(existingRace)
+                    : "There should be a Race with the same name in this.raceInstances";
+
                 throw new IllegalNameException(String.format("Race name %s already exists", name));
             }
         } catch (NameNotRecognisedException e) {
@@ -104,7 +108,15 @@ public class CyclingPortalImpl implements CyclingPortal {
         }
         
         Race race = new Race(name, description);
+
+        assert !this.raceInstances.contains(race)
+            : "There should not be any existing references to a brand new Race";
+
         this.raceInstances.add(race);
+
+        assert this.raceInstances.contains(race)
+            : "The new Race should have been appended to this.raceInstances";
+
 		return race.getId();
 	}
 
@@ -118,6 +130,14 @@ public class CyclingPortalImpl implements CyclingPortal {
 	public void removeRaceById(int raceId) throws IDNotRecognisedException {
 		Race race = findRace(raceId);
         this.raceInstances.remove(race);
+
+        assert this.raceInstances.contains(race)
+            : "The Race selected for removal should exist in this.raceInstances";
+
+        this.raceInstances.remove(race);
+
+        assert !this.raceInstances.contains(race)
+            : "There should not be any references to a removed Race";
 	}
 
 	@Override

src/cycling/Race.java
@@ -102,18 +102,35 @@ public class Race {
 
     public void addStage(Stage stage) throws IllegalNameException {
         try {
-            if (Stage.findStageByName(this.stages, name) instanceof Stage) {
-                throw new IllegalNameException(String.format("Stage name %s already exists", name));
+            Stage existingStage = Stage.findStageByName(this.stages, stage.getName());
+
+            if (existingStage instanceof Stage) {
+                assert this.stages.contains(existingStage)
+                    : "There should be a Stage with the same name in this.stages";
+                
+                throw new IllegalNameException(String.format("Stage name %s already exists", stage.getName()));
             }
         } catch (NameNotRecognisedException e) {
             // Do nothing - name is unique
         }
 
+        assert !this.stages.contains(stage)
+            : "There should not be any existing references to a brand new Stage";
+
         this.stages.add(stage);
+
+        assert this.stages.contains(stage)
+            : "The new Stage should have been appended to this.stages";
     }
 
     public void removeStage(Stage stage) {
+        assert this.stages.contains(stage)
+            : "The Stage selected for removal should exist in this.stages";
+
         this.stages.remove(stage);
+
+        assert !this.stages.contains(stage)
+            : "There should not be any references to a removed Stage";
     }
 
 

src/cycling/Stage.java
@@ -136,7 +136,13 @@ public class Stage {
             throw new InvalidLocationException(String.format("Checkpoint location %f must be less than the Stage length %f", checkpoint.getLocation(), this.length));
         }
 
+        assert !this.checkpoints.contains(checkpoint)
+            : "There should not be any existing references to a brand new Checkpoint";
+
         this.checkpoints.add(checkpoint);
+
+        assert this.checkpoints.contains(checkpoint)
+            : "The new Checkpoint should have been appended to this.checkpoints";
     }
     
     public void removeCheckpoint(Checkpoint checkpoint) throws InvalidStageStateException {
@@ -144,7 +150,13 @@ public class Stage {
             throw new InvalidStageStateException(String.format("Stage state cannot be %s", this.state));
         }
         
+        assert this.checkpoints.contains(checkpoint)
+            : "The Checkpoint selected for removal should exist in this.checkpoints";
+
         this.checkpoints.remove(checkpoint);
+
+        assert !this.checkpoints.contains(checkpoint)
+            : "There should not be any references to a removed Checkpoint";
     }
 
 

commit 3802725e2b5d94ccf4038890aff9ead0d858e64f

fix #18 by removing duplicate raceInstances.remove caused by 3a7656cf0d73605fd89748df6b85597175e9ebe3

src/cycling/CyclingPortalImpl.java
@@ -163,7 +163,6 @@ public class CyclingPortalImpl implements CyclingPortal {
 	@Override
 	public void removeRaceById(int raceId) throws IDNotRecognisedException {
 		Race race = findRace(raceId);
-        this.raceInstances.remove(race);
 
         assert this.raceInstances.contains(race)
             : "The Race selected for removal should exist in this.raceInstances";
@@ -405,7 +404,6 @@ public class CyclingPortalImpl implements CyclingPortal {
 	@Override
 	public void removeRaceByName(String name) throws NameNotRecognisedException {
 		Race race = Race.findRaceByName(this.raceInstances, name);
-        this.raceInstances.remove(race);
 
         assert this.raceInstances.contains(race)
             : "The Race selected for removal should exist in this.raceInstances";