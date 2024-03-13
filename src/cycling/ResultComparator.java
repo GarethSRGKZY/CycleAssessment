96933dc23a4ca8a39750f2ddef0996438fdb4801
SequentialEntropy <asahi79289@gmail.com>
2024-03-13 18:02:30 +0000

    update ResultComparator to support getElapsedTimeToCheckpoint from 22c1700fac8eaca9c84b741ce7f359da9122a274

--- src/cycling/ResultComparator.java
+++ src/cycling/ResultComparator.java
@@ -3,8 +3,23 @@ package cycling;
 import java.util.Comparator;
 
 public class ResultComparator implements Comparator<Result> {
+    // Static Attributes
+    private int checkpointIndex;
+
+    // Instance Methods
+    public ResultComparator(int checkpointIndex) { // Constructor
+        this.checkpointIndex = checkpointIndex;
+    }
+
+    public ResultComparator() { // Constructor (Overloaded)
+        this(-1);
+    }
+
     @Override
     public int compare(Result result1, Result result2) {
-        return result1.getElapsedTime().compareTo(result2.getElapsedTime());
+        if (this.checkpointIndex == -1) {
+            return result1.getElapsedTime().compareTo(result2.getElapsedTime());
+        }
+        return result1.getElapsedTimeToCheckpoint(checkpointIndex).compareTo(result2.getElapsedTimeToCheckpoint(checkpointIndex));
     }
 }
