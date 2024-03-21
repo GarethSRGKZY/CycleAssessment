af56c0595a4bccb15d1c4c3b6b3e13ac7b5fa0ae
GarethSRGKZY <divinegareth1313@gmail.com>
2024-03-22 05:41:40 +0800

    added "--source 11 --target 11" to compile scripts

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ mkdir doc
 
 javadoc -d doc/ src/cycling/*.java
 
-javac -d bin/ src/cycling/*.java
+javac --source 11 --target 11 -d bin/ src/cycling/*.java
 
 jar cvf build/cycling.jar -C bin .
 jar uvf build/cycling.jar -C src .
